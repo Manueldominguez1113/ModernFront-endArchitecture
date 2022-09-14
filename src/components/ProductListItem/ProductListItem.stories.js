2efa1bc26d0d0b4e81da54cf10940b15bc81255d
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-09-13 20:49:29 -0500

    typo

--- src/components/ProductListItem/ProductListItem.stories.js
+++ src/components/ProductListItem/ProductListItem.stories.js
@@ -4,4 +4,4 @@ import ProductListItem from "./ProductListItem";
 
 storiesOf('Product List Item', module)
 
-.add('standard',()=><ProductListItem/>); //StoriesOf is a component of storybook, maing it easier than ever to create storyboards. using a simple add function to name the storyboard, and a function of where it's from.
+.add('standard',()=><ProductListItem/>); //StoriesOf is a component of storybook, making it easier than ever to create storyboards. using a simple add function to name the storyboard, and a function of where it's from.
