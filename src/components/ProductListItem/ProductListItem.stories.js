ea8c9eef7e291470e811a048551752e311585e29
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-09-16 00:03:37 -0500

    changed to component stories method

--- src/components/ProductListItem/ProductListItem.stories.js
+++ src/components/ProductListItem/ProductListItem.stories.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import ProductListItem from "./ProductListItem";
+
+export default {title: 'ProductListItem'};
+
+export const standard = ()=> <ProductListItem/>
+
+
+
+/*  preform stories. use component stories as seen above
+import React from 'react';
 import { storiesOf } from "@storybook/react";
 import ProductListItem from "./ProductListItem";
 
 storiesOf('Product List Item', module)
 
 .add('standard',()=><ProductListItem/>); //StoriesOf is a component of storybook, making it easier than ever to create storyboards. using a simple add function to name the storyboard, and a function of where it's from.
+*/
